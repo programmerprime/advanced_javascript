c9cd5e23cf79a687184b44f50438beabede45428
programmerprime <tmai.prime@gmail.com>
2020-08-07 10:51:57 +0600

    no

---
+++ truthy_falsy.js
@@ -0,0 +1,45 @@
+// javascript will consider them as falsy values:
+
+// 0, "", undefined, null, NaN, boolean false
+
+// javascript will consider them as true values
+
+// 1, "0", " ", empty array, empty object, boolean true
+
+
+
+
+// const name = "";
+
+// if(name) {
+//     console.log("condition is true");
+// } else {
+//     console.log("condition is false");
+// }
+
+// const age = 0;
+
+// if(age) {
+//     console.log("condition is true");
+// } else {
+//     console.log("condition is false");
+// }
+
+// const age = "0";
+
+// if(age) {
+//     console.log("condition is true");
+// } else {
+//     console.log("condition is false");
+// }
+
+
+let name;
+
+console.log(name);
+
+if(name) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
