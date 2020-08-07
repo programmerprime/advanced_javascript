1102d3b7798f892ce9a62dc5b2e327df2543e831
programmerprime <tmai.prime@gmail.com>
2020-08-07 11:53:43 +0600

    no message

---
+++ null_undefined.js
@@ -0,0 +1,26 @@
+// let myObj = {name: "Prime", age: 19};
+
+// console.log(myObj.department);
+
+// let x;
+
+// console.log(x);
+
+// let arr = [2, 3, 4];
+
+// console.log(arr[8]);
+
+// function add(num1, num2) {
+//     console.log(num1, num2);
+// }
+
+// add(20);
+
+
+function subtraction(a, b) {
+    let n = a - b;
+}
+
+let result = subtraction(20, 10);
+
+console.log(result); // the function is not returning anything, so the result will undefined
